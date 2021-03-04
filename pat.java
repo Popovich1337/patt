115a4bef26c73338c19bc9196c5b8da80ac27676
Popovich1337 <71813908+Popovich1337@users.noreply.github.com>
2021-03-04 12:23:47 +0200

    pat.java

---
+++ pat.java
@@ -0,0 +1,10 @@
+public class pat {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello");
+        builder.append(',');
+        builder.append("World!");
+        System.out.println(builder.toString());
+    }
+}
+
